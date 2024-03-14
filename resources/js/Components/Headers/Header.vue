3f385a580c4531d215e084769170ddebd74cf105
Zachary Turner <zakkturner@yahoo.com>
2024-03-14 14:07:05 -0400

    first commit

---
+++ resources/js/Components/Headers/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="-left-1/2 fixed h-screen bg-[#51acfb]">
+        <div class="text-center px-10">
+            <div><application-logo></application-logo></div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Plumbing</a>
+                    </li>
+                    <li>
+                        <a href="#">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import ApplicationLogo from "../ApplicationLogo.vue";
+</script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/Components/Headers/MobileHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bg-[rgba(255,255,255,.2)] w-full shadow-lg">
+        <div class="flex items-center px-6">
+            <div class="mr-2">
+                <div @click="isOpen" class="w-6">
+                    <div class="h-1 bg-white my-1"></div>
+                    <div class="h-1 bg-white my-1"></div>
+                    <div class="h-1 bg-white my-1"></div>
+                </div>
+            </div>
+            <div class="w-20">
+                <img
+                    src="http://prohauz.local/wp-content/uploads/2018/09/logo-white.png"
+                    alt="Logo"
+                    srcset=""
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import gsap from "gsap";
+function isOpen() {
+    alert("Opened");
+}
+</script>
+
+<style lang="scss" scoped></style>
